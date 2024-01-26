e695cc25522f6ca8415b445f72ad326b4dc2d641
Anton Rozdorozhniuk <anton.rozdorozhniuk@futureplatforms.com>
2024-01-26 16:30:23 +0200

    adding missing localization files

---
+++ dependencies_app/fib/flavor/lib/localization/generated/app_localizations_en.dart
@@ -0,0 +1,9 @@
+import 'app_localizations.dart';
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get title => 'Press the button to add 1 to the counter';
+}
